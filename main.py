131982951f174b54d30b8fc53e6cddc0b4d520f6
Nagatani <nagatani@work-ark.jp>
2021-11-18 15:44:19 +0900

    Add Diary(これはコンフリクトが発生する変更)

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from diaries.DiarySample import DiarySample
 from diaries.NagataniDiary import NagataniDiary
+from diaries.Nagata2Diary import Nagata2Diary
 
 # ↓のリストには、メンバーの各日記が格納されます。
 diaries = [
-  DiarySample(),
-  NagataniDiary(),
-] 
+    DiarySample(),
+    NagataniDiary(),
+    Nagata2Diary(),
+]
 
 for d in diaries:
     print("---------------------------------")
